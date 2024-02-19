ed9f4758a19535f6bd38730aeedd599d9958829a
tomo0611 <tomo0611@hotmail.com>
2024-02-19 20:00:45 +0900

    rootコマンド完成

--- cmd/root.go
+++ cmd/root.go
@@ -10,23 +10,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
+// rootCmdはサブコマンドなしでじっこうしたときに呼ばれるコマンドです。
 var rootCmd = &cobra.Command{
 	Use:   "tomo-wallet",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "ボクのお財布の履歴を管理するためのCLIツールです。",
+	Long:  `ボクのお財布の履歴を管理するためのCLIツールです。`,
+	// 実行時に何か処理をさせたければこれをコメントアウト
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// Executeは全てのサブコマンドとフラグをルートコマンドに追加する。
+// main.main()によって呼び出され、rootCmdに対して一度だけ呼び出される必要がある。
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -35,13 +29,13 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	// フラグと設定を定義する
+	// Cobraは、ここで定義されるとアプリケーション内でグローバルになる
+	// 永続フラグを使うことができる。
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tomo-wallet.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	// Cobraはこのアクションが呼び出された時にだけ実行される
+	// ローカルフラグも使うことができる。
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
